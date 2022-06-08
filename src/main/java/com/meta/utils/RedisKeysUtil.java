37fa3a3a1b676b9da92ed4ef7d13afac4a6e926e
huapeiliang <2473095423@qq.com>
2022-06-08 23:28:37 +0800

    bug fix

--- src/main/java/com/meta/utils/RedisKeysUtil.java
+++ src/main/java/com/meta/utils/RedisKeysUtil.java
@@ -1,13 +1,29 @@
 package com.meta.utils;
 
+import com.meta.model.enums.VerificationMethodEnum;
+import com.meta.model.enums.VerificationScenarioEnum;
+
 public class RedisKeysUtil {
 
+    /**
+     * 绑定手机模糊 key
+     * */
     public static String bindingPhoneLikeKey(String areaCode, String phone){
         return "BINDING_PHONE_SMS_CODE"+ areaCode + phone;
     }
 
+    /**
+     * 绑定手机 key
+     * */
     public static String bindingPhoneKey(String areaCode, String phone, String code){
         return "BINDING_PHONE_SMS_CODE"+ areaCode + phone + ":" + code;
+    }
+
+    /**
+     * 验证码倒计时
+     */
+    public static String sendVerificationCodeCountdown(VerificationMethodEnum verificationMethod, VerificationScenarioEnum verificationScenario, String value){
+        return "SEND_VERIFICATION_CODE_COUNTDOWN:" + verificationMethod.getValue() + ":" + verificationScenario.getValue() + ":" + value;
     }
 
 
